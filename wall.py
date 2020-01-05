2b5e86566591d781065fe2e73f35c95ca564786d
Matthew Yu <matthew@skr-skr.local>
2020-01-05 23:49:22 +0800

    added database

--- wall.py
+++ wall.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for
+import sqlite3
+connection =  sqlite3.connect("posts.sql")
+cursor = connection.cursor()
 
 app = Flask(__name__)
-posts = [{"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}]
+#posts = [{"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}, {"author" : "hehe", "date_posted" : "2019", "title": "haha", "content": "hoho"}]
+cursor.execute("SELECT * FROM posts;")
+posts = cursor.fetchall() 
 
 
 @app.route('/')
